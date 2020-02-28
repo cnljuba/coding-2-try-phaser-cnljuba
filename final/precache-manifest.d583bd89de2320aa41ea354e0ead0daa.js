eb4a19faa318dcedf34e52b4acc5ee6ed1a0f069
Casey <casey.ljuba@gmail.com>
2020-02-28 14:28:49 -0500

    Updates

---
+++ final/precache-manifest.d583bd89de2320aa41ea354e0ead0daa.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/citations.md"
+  },
+  {
+    "revision": "89cf69a9a0c97e87d99301c1b5ee779a",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/background.png"
+  },
+  {
+    "revision": "62b86a0a8d00f100034a2c17cc5b84f3",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/far.png"
+  },
+  {
+    "revision": "ad58890a3177fc0275e8e75708c350fc",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/food.png"
+  },
+  {
+    "revision": "9e513dc66aa67c298869249c81d36b14",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/foregound-merged.png"
+  },
+  {
+    "revision": "653fd4e1b45bcb6f9ee31c6ab3df8273",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/foreground-1.png"
+  },
+  {
+    "revision": "d478305e59404c6309392a24e70a149f",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/foreground-2.png"
+  },
+  {
+    "revision": "9273e642fd82b708d44eab55ec226f3c",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/ray.gif"
+  },
+  {
+    "revision": "4d7bfadbde5e25ca3b106ae37d98fe30",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/raysprite.png"
+  },
+  {
+    "revision": "97eb75dc544b864964a8a75d0e135fab",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/sand.png"
+  },
+  {
+    "revision": "37bdbf2ef734a28c4d50306a052f56e0",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/shark.png"
+  },
+  {
+    "revision": "ccd0f68c9dc7cd68ea1e98bf43aa7c90",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/ship.png"
+  },
+  {
+    "revision": "5fa6428a237404ad4bd8",
+    "url": "/coding-2-try-phaser-cnljuba/finalgen.main.96bb8abc0764e04a345f.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-cnljuba/finalgen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "7f8a79a84042b37f31303c465529bc6d",
+    "url": "/coding-2-try-phaser-cnljuba/finalindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-cnljuba/finalmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-cnljuba/finalsw.js"
+  }
+]);
