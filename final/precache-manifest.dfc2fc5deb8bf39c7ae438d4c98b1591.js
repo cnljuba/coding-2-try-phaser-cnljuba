7f25c4e28dec50b149d34758752c8947c6971890
Casey <casey.ljuba@gmail.com>
2020-03-06 21:51:35 -0500

    Updates

---
+++ final/precache-manifest.dfc2fc5deb8bf39c7ae438d4c98b1591.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/citations.md"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/font/font.png"
+  },
+  {
+    "revision": "38c02ad8ccaf9579150bb10d283e2b0c",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/font/font.xml"
+  },
+  {
+    "revision": "e10a71cbcb12bb2708a938308441f7a3",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/1.png"
+  },
+  {
+    "revision": "01584f6172f809d4dd42064a64945d28",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/2.png"
+  },
+  {
+    "revision": "0b0e621156ff8aeb30443becb693b78c",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/3.png"
+  },
+  {
+    "revision": "d58c8215eb99df87e29172a9e7ae0a64",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/4.png"
+  },
+  {
+    "revision": "4556194d85b53330be346fb018e529fa",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/5.png"
+  },
+  {
+    "revision": "0278e3b78295503904d9a0d7d909c0d6",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/6.png"
+  },
+  {
+    "revision": "15b5453d1d18c8ffe7eae619b491b478",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/7.png"
+  },
+  {
+    "revision": "2951b9032518ad4d635a87c123e2ba75",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/8.png"
+  },
+  {
+    "revision": "e94012ce6ef9befc92aea1c0dd5e012e",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/background.png"
+  },
+  {
+    "revision": "89cf69a9a0c97e87d99301c1b5ee779a",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/backgroundd.png"
+  },
+  {
+    "revision": "62b86a0a8d00f100034a2c17cc5b84f3",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/far.png"
+  },
+  {
+    "revision": "ad58890a3177fc0275e8e75708c350fc",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/food.png"
+  },
+  {
+    "revision": "9e513dc66aa67c298869249c81d36b14",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/foregound-merged.png"
+  },
+  {
+    "revision": "653fd4e1b45bcb6f9ee31c6ab3df8273",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/foreground-1.png"
+  },
+  {
+    "revision": "d478305e59404c6309392a24e70a149f",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/foreground-2.png"
+  },
+  {
+    "revision": "9273e642fd82b708d44eab55ec226f3c",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/ray.gif"
+  },
+  {
+    "revision": "5316eafffb3116648a6ec5a9a0871220",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/raysprite.png"
+  },
+  {
+    "revision": "97eb75dc544b864964a8a75d0e135fab",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/sand.png"
+  },
+  {
+    "revision": "37bdbf2ef734a28c4d50306a052f56e0",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/shark.png"
+  },
+  {
+    "revision": "ccd0f68c9dc7cd68ea1e98bf43aa7c90",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/ship.png"
+  },
+  {
+    "revision": "5316eafffb3116648a6ec5a9a0871220",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/sprite.png"
+  },
+  {
+    "revision": "3b687bfcf4bc7913334cf1bef3498358",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/sprite1.png"
+  },
+  {
+    "revision": "39e0a216cedc221a8aa26bea06bf3413",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/images/sprite2.png"
+  },
+  {
+    "revision": "86b88d1e4615b77a8b94a93ba82500bf",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/sounds/beam.mp3"
+  },
+  {
+    "revision": "ce33e3b835ac69e0766dbe6d7c67dfc1",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/sounds/beam.ogg"
+  },
+  {
+    "revision": "ce8dbd56bcb81632df7279e91fdcec54",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/sounds/explosion.mp3"
+  },
+  {
+    "revision": "4b9be0c2fdf194385275c2e8d006949b",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/sounds/explosion.ogg"
+  },
+  {
+    "revision": "2101f87289e14406eb4b215cf55a3ebc",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/sounds/pickup.mp3"
+  },
+  {
+    "revision": "2caf90e1d1576c0a7891bfb9723ded61",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/sounds/pickup.ogg"
+  },
+  {
+    "revision": "99053fcb5d85a0575a3958a8a3390433",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/sounds/rain.mp3"
+  },
+  {
+    "revision": "da087af3292fb062136a96eb496ceaf7",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/sounds/sci-fi_platformer12.mp3"
+  },
+  {
+    "revision": "240502f3ab4e1955dd49c1dbf827e247",
+    "url": "/coding-2-try-phaser-cnljuba/finalassets/sounds/sci-fi_platformer12.ogg"
+  },
+  {
+    "revision": "06104c6480b8a7cad355",
+    "url": "/coding-2-try-phaser-cnljuba/finalgen.main.91dbb4af2a2fd7caaca0.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-cnljuba/finalgen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "007a98565d55d29ad28910bc28840d1b",
+    "url": "/coding-2-try-phaser-cnljuba/finalindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-cnljuba/finalmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-cnljuba/finalsw.js"
+  }
+]);
--- final/sw.js
+++ final/sw.js
@@ -1,4 +1,4 @@
-importScripts("/coding-2-try-phaser-cnljuba/finalprecache-manifest.d583bd89de2320aa41ea354e0ead0daa.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/coding-2-try-phaser-cnljuba/finalprecache-manifest.dfc2fc5deb8bf39c7ae438d4c98b1591.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
